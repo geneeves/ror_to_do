b9e62ec734bef897315a8a0772291b81dbeb66bb
epicodus <student@epicodus.com>
2013-09-24 14:30:29 -0700

    add lists migration and updated tasks and schema

--- db/migrate/20130924000221_create_tasks.rb
+++ db/migrate/20130924000221_create_tasks.rb
@@ -2,6 +2,8 @@ class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
       t.string :description
+      t.integer :list_id
+      
       t.timestamps
     end
   end
